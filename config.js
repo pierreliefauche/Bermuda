4bc9c092c3ed3713abe3010e55344cf838f00f63
Pierre-Élie Fauché <github@pierre-elie.me>
2016-01-08 00:08:29 +0100

    config can be set in environment

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Your Google API key, if you want to use one.
-  apiKey: null,
+  apiKey: process.env.API_KEY,
 
   // Restrict URLs that can be shortened by the service.
   // Can be a string, in which case URL to shorten must start with this string,
   // or it can be a regular expression object. I love javascript.
-  restrict: null
+  restrict: process.env.RESTRICT
 };
